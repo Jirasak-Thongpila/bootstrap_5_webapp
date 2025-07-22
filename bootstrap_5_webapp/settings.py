916c9469c75ea411aad5e9b8bb7ec5ec5328cba7
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-07-22 22:29:09 +0700

    fix config 2

--- bootstrap_5_webapp/settings.py
+++ bootstrap_5_webapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e$ng#=9+cf2r+s_p8uk3*+k$xuu1fqs(x#j@)_%s^16x*h55l-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True  # เปิด DEBUG เพื่อดู error
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']  # อนุญาตทุก host
 
 
 # Application definition
@@ -118,14 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# ลบ STATICFILES_DIRS ออกก่อน เพราะไม่มี folder static
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
