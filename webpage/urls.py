c1a59add2500e6434b382e7b9b0680f449e32783
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-08-04 11:05:41 +0700

    เพิ่มฟังก์ชันสูตรคูณและสร้างเทมเพลตสำหรับแสดงผลลัพธ์

--- webpage/urls.py
+++ webpage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),   
     path('for/', views.for_view, name='for'),
+    path('multipli/', views.multipli, name='multipli'),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -22,3 +22,17 @@ def for_view(request):
         context['count'] = list(range(1, 2))
         
     return render(request, 'for.html',context)
+
+def multipli(request):
+    context = {}
+    context['message'] = "ตารางสูตรคูณ"
+    
+    if request.method == 'POST' and request.POST.get('number') != '':
+        number = int(request.POST.get('number'))
+        context['number'] = number
+        context['table'] = [(i, number * i) for i in range(1, 13)]
+    else:
+        context['table'] = []
+        context['number'] = None
+        
+    return render(request, 'table.html', context)
